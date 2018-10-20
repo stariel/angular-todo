faa33e673e89fa81f2ab276bb344bd315b7adbfd
Ariel Altaras <ariel.altaras@gmail.com>
2018-10-20 16:08:47 -0700

    created list, items, and added styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
   template: `
-  <h1>
-    Welcome to {{ title }}!
-  </h1>
-  
-  <app-input-button-unit></app-input-button-unit>
-`,
+    <h1 class="app-title">
+      Welcome to {{ title }}!
+    </h1>
+
+    <app-list-manager></app-list-manager>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'My To Do List';
 }
